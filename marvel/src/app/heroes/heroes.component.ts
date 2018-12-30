2e8c4f90ff600e55714d5909a49940c33c621c0b
abhijithneilabraham <abhijithneilabrahampk@gmail.com>
2018-12-31 00:15:33 +0530

    something wrong

--- marvel/src/app/heroes/heroes.component.ts
+++ marvel/src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  hero: Hero = {
+    id: 1,
+    name: 'Tony Stark'
+  };
 
   constructor() { }
 
