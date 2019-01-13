6dacf27953c0770ba00c3c9fca930ccb8de2a38c
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-01-13 23:21:04 +0530

    commit pullup

--- Avengers/src/app/heroes/heroes.component.ts
+++ Avengers/src/app/heroes/heroes.component.ts
@@ -8,14 +8,16 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 'Human',
-    name: 'Tony Stark'
-  };
+
   heroes = HEROES;
+  selectedHero: Hero;
   constructor() { }
 
+
   ngOnInit() {
+  }
+  onSelect(hero:Hero):void {
+    this.selectedHero=hero;
   }
 
 }
