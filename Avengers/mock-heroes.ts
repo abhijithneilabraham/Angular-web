f29e351653ccadfe18a71da7c3fcafdb1ead5fad
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-01-13 22:54:47 +0530

    update

---
+++ Avengers/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Hulk' },
+  { id: 12, name: 'Natasha Romanoff' },
+  { id: 13, name: 'Captain America' },
+  { id: 14, name: 'Thor' },
+  { id: 15, name: 'Black Panther' }
+];
