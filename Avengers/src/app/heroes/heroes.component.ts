855af2ff3af69a14dcdfd849dff4ae038a5468da
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-01-13 20:24:30 +0530

    huh

---
+++ Avengers/src/app/heroes/heroes.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+hero ='Tony Stark' 
+})
+export class HeroesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
