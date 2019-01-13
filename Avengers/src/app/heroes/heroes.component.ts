706167f3ddcdde367af2ad803535d6ba93b46b17
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-01-13 20:43:22 +0530

    ha yeah

--- Avengers/src/app/heroes/heroes.component.ts
+++ Avengers/src/app/heroes/heroes.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css'],
-hero ='Tony Stark' 
+  styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  hero: Hero = {
+    id: 1,
+    name: 'Tony Stark'
+  };
 
   constructor() { }
 
